7a991a6df4a1e88e5f55f7a6c760b638edeb9454
Kacper Barczyk <kacper1409@gmail.com>
2022-03-18 16:56:40 +0100

    added Controller and Service tested controller via postman

--- src/main/java/com/lab2/controller/BrowserRequestController.java
+++ src/main/java/com/lab2/controller/BrowserRequestController.java
@@ -1,11 +1,19 @@
 package com.lab2.controller;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+import org.springframework.http.ResponseEntity;
 import org.springframework.stereotype.Controller;
 import org.springframework.web.bind.annotation.GetMapping;
 
 @Controller
 public class BrowserRequestController {
 
-    @GetMapping(value = "/broweser-")
+    Logger logger = LoggerFactory.getLogger(BrowserRequestController.class);
 
+    @GetMapping("/controller")
+    public ResponseEntity<String> testGetController() {
+        logger.info("controller works");
+        return ResponseEntity.ok("Reply");
+    }
 }
---
+++ src/main/java/com/lab2/service/BrowserRequestService.java
@@ -0,0 +1,10 @@
+package com.lab2.service;
+
+import org.springframework.stereotype.Service;
+
+@Service
+public class BrowserRequestService {
+
+
+
+}
